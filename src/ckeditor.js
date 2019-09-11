2a1432c04f3cd24e0cbe91d9ab4736f6b3944af8
nelsh <nik.elshanski@outlook.com>
2019-09-11 15:09:24 +0800

    feat: add plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -15,9 +15,14 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
@@ -26,6 +31,7 @@ import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
@@ -33,9 +39,10 @@ import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent'
-import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock'
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount';
 
 class ClassicEditor extends ClassicEditorBase {}
@@ -48,6 +55,7 @@ const plugins = [
 	Autoformat,
 	Bold,
 	Italic,
+	Strikethrough, Code, Subscript, Superscript,
 	BlockQuote,
 	CKFinder,
 	EasyImage,
@@ -57,8 +65,9 @@ const plugins = [
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
+	ImageResize,
 	Link,
-	List,
+	List, TodoList,
 	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
@@ -68,6 +77,7 @@ const plugins = [
 	Alignment,
 	Indent, IndentBlock,
 	RemoveFormat,
+	TextTransformation,
 	WordCount
 ];
 
